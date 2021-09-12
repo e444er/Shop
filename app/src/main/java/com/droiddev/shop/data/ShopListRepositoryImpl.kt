613e48a3e1085e83cec9551559455e0337de7341
e444er <e444er@@gmail.com>
2021-09-12 13:18:49 +0600

    ShopListRepositoryImpl

---
+++ app/src/main/java/com/droiddev/shop/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.droiddev.shop.data
+
+import com.droiddev.shop.domain.ShopItem
+import com.droiddev.shop.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+}
