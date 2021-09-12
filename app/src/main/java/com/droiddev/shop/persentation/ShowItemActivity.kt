acf25dcff06feed63fbd9b7a4521d86ef5551e64
e444er <e444er@@gmail.com>
2021-09-12 23:05:43 +0600

    ShowItemActivity

---
+++ app/src/main/java/com/droiddev/shop/persentation/ShowItemActivity.kt
@@ -0,0 +1,12 @@
+package com.droiddev.shop.persentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.droiddev.shop.R
+
+class ShowItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_item)
+    }
+}
